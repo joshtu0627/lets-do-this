19b47918d3f55e9e1251b1ab1a0cd0955ae723b3
Josh <joshtu0627@gmail.com>
2023-12-14 23:11:18 +0800

    123

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -1,3 +1,5 @@
 export const frontendurl = "http://localhost:5173/";
-export const backendurl = "localhost:8000/api/1.0";
-export const socketurl = "localhost:8000";
+// export const backendurl = "localhost:8000/api/1.0";
+// export const socketurl = "localhost:8000";
+export const backendurl = "13.236.23.10:8000/api/1.0";
+export const socketurl = "13.236.23.10:8000";
